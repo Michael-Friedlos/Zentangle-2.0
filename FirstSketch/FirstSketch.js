ff801633d108b318e857b63b0c669ed42d5265be
Michael-Friedlos <michael.friedlos@edu.tbz.ch>
2022-10-26 16:55:41 +0200

    neu

--- FirstSketch/FirstSketch.js
+++ FirstSketch/FirstSketch.js
@@ -1,61 +1,16 @@
 function setup() {
-createCanvas(windowWidth, windowHeight);
-rectMode(CENTER);
-noFill();
-stroke(0);
+  createCanvas(windowWidth, windowHeight);
+  background(100);
 }
-
-
-
-
-
-
-
-//jgugcui
-
 
 function draw() {
-cellWidth = windowWidth / gridX;
-cellHeight = windowHeight / gridY;
-background(0);
-for (let i = 0; i <= gridX; i++) {
-for (let j = 0; j <= gridY; j++) {
-if (j % 100 == 100) {
-drawPattern(i * cellWidth, j * cellHeight, cellWidth, cellHeight, innerCells);
-} else {
-drawPattern(i * cellWidth - cellWidth * 0.5, j * cellHeight, cellWidth, cellHeight, innerCells);
-}}}}
-
-
-
-function drawPattern(x, y, w, h, amount) {
-for(let i = 0; i <= amount; i++) {
-let width = i * w / amount;
-let height = i * h / amount;
-strokeWeight(width * 0.01);
-rect(x, y, width, height);
-}}
-
-
-
-function mouseClicked() {
-let min = 1;
-let max = 10;
-innerCells = min + getRandomInt(max);
-gridX = min + getRandomInt(max);
-gridY = min + getRandomInt(max);
-stroke(random(100,200), random(100,200), random(100,200));
+  translate(width/2,height/2)
+  rotate(frameCount)
+  translate(frameCount,0)
+  if(frameCount%5<3){
+    fill(53-random(200), 80, 112)
+  }else{
+    fill(229-random(200), 107, 111)  
+  }
+  rect(0,0,10,10)
 }
-
-
-
-function getRandomInt(max) {
-return Math.floor(Math.random() * max);
-}
-
-
-
-let gridX = 10;
-let gridY = 10;
-let innerCells = 10;
-let cellWidth, cellHeight;
